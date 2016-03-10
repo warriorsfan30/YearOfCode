8d9514dfa679e9d13d59be5b0e651e9d79e7e032
Anish Kasam <iamalonerstalker@gmail.com>
2016-03-09 18:19:10 -0800

    Update dice.js

--- projects/js/dice.js
+++ projects/js/dice.js
@@ -2,3 +2,13 @@ $(".dice1").click(function() {
     var dice1Probability = Math.floor(Math.random() * 6 + 1);
     $(this).html(dice1Probability);
 });
+
+$(".addDice").click(function() {
+    $(this).removeClass("add");
+    $(this).addClass("dice1");
+    $(this).html("Roll");
+    $(".dice1").click(function() {
+        var dice1Probability = Math.floor(Math.random() * 6 + 1);
+        $(this).html(dice1Probability);
+    });
+});
