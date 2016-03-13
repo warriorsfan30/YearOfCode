aab1044e3a36c4941dd56745b43177f7c597567d
Anish Kasam <iamalonerstalker@gmail.com>
2016-03-13 14:18:10 -0700

    Create savedclicks.js

---
+++ projects/js/savedclicks.js
@@ -0,0 +1,14 @@
+function clickCount() {
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("clicks").innerHTML = localStorage.clickcount;
+    } else {
+        document.getElementById("clicks").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
+
+document.getElementById("clicks").innerHTML = localStorage.clickcount;
