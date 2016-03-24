2ea255203d53b4305d4e905de426ca6549816da0
Anish Kasam <iamalonerstalker@gmail.com>
2016-03-23 17:02:23 -0700

    Create tryclicking.js

---
+++ projects/js/tryclicking.js
@@ -0,0 +1,17 @@
+var Button = $(".randomPos");
+
+function random(number) {
+  		return Math.floor(Math.random() * number);
+}
+
+Button.on("mouseenter", function() {
+	$(".randomPos").css({
+      		position: "absolute",
+      		top: random(75) + "%",
+      		left: random(75) + "%"
+  		});
+});
+
+Button.on("click", function() {
+  		alert("Congratulations! You clicked the button! You must have illegally fast reflexes or you changed the code for this program!");
+});
