a508343d30c25587539d2fc30189b9296d4b5a0e
Anish Kasam <iamalonerstalker@gmail.com>
2016-04-15 17:44:20 -0700

    Create warriors.js

---
+++ projects/js/warriors.js
@@ -0,0 +1,30 @@
+var randomColor = function() {
+    return "#" + Math.random().toString(16).slice(2, 8);
+}
+
+function random(number) {
+    return Math.floor(Math.random() * number);
+}
+
+var color = randomColor();
+var gameswon = 0;
+
+$("#current").click(function() {
+    color = randomColor();
+
+    $("#current").css({
+        position: "absolute",
+        top: random(85) + "%",
+        left: random(90) + "%"
+    });
+    
+    $("#current").html(gameswon);
+    
+    gameswon = gameswon + 1;
+    
+    $("#gameswon").html(gameswon);
+    
+    if (eggsfound % 73 == 0) {
+        alert("The Warriors set the record for the most games won in a season!");
+    }
+});
