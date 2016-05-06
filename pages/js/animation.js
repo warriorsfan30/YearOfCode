1cd5d3bcffc940b985730933052a9a5864fd661d
Anish Kasam <iamalonerstalker@gmail.com>
2016-05-06 16:30:16 -0700

    Update animation.js

--- pages/js/animation.js
+++ pages/js/animation.js
@@ -8,16 +8,34 @@ $(document).ready(function() {
     $("content").fadeIn(1000);
 });
 
-$(document).keydown(function (e) {
-    if (e.keyCode == 9) {
-        alert("Edit Some Text!");
-        document.getElementsByTagName("BODY")[0].setAttribute("contenteditable", "true");
-    }
-});
-
 $(".nav-icon").click(function() {
     $(this).toggleClass("active");
     $(".menu").toggleClass("active");
 });
 
 var wow = new WOW().init();
+
+var musickeys = [];
+var music = '77,85,83,73,67';
+
+$(document).keydown(function(e) {
+    musickeys.push(e.keyCode);
+    if (musickeys.toString().indexOf(music) >= 0) {
+        musickeys = [];
+        alert('Music!')
+        document.getElementsByTagName("HTML")[0].setAttribute("contenteditable", "true");
+        $('body').addClass('background').append('<iframe width="0" height="0" src="https://www.youtube.com/embed/EZpOLOLbwJw?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+    }
+});
+
+var editkeys = [];
+var edit = '69,68,73,84';
+
+$(document).keydown(function(e) {
+    editkeys.push(e.keyCode);
+    if (editkeys.toString().indexOf(music) >= 0) {
+        editkeys = [];
+        alert('Edit the Text!')
+        document.getElementsByTagName("BODY")[0].setAttribute("contenteditable", "true");
+    }
+});
