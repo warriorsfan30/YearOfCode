8c8eb35772610b90be29edbb4c6242736a3047f4
Anish Kasam <iamalonerstalker@gmail.com>
2016-06-04 10:29:18 -0700

    Update animation.js

--- pages/js/animation.js
+++ pages/js/animation.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
-    $("h1").hide();
-    $("p").hide();
-    $("content").hide();
-    
-    $("h1").fadeIn(1000);
-    $("p").fadeIn(1000);
-    $("content").fadeIn(1000);
+    $("body").hide();
+    $("body").fadeIn(500);
 });
 
 $(".nav-icon").click(function() {
