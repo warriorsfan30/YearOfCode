62cba83c11bc6df909d896d936467fad29dbbc2a
Anish Kasam <iamalonerstalker@gmail.com>
2016-06-12 12:10:24 -0700

    Update animation.js

--- pages/js/animation.js
+++ pages/js/animation.js
@@ -8,6 +8,27 @@ $(".nav-icon").click(function() {
     $(".menu").toggleClass("active");
 });
 
+$(".menu a").click(function() {
+    $(".nav-icon").click();
+});
+
+// Credit for the smooth-scroll effect goes to CSS-Tricks
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
 var wow = new WOW().init();
 
 var keys = [];
