c24e67f1592bf86ac672a1fcbf61ea186e763585
Anish Kasam <iamalonerstalker@gmail.com>
2016-06-19 20:19:38 -0700

    Create slideshowtwo.js

---
+++ projects/css/slideshowtwo.js
@@ -0,0 +1,40 @@
+body {
+    background-image: url('');
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    background-color: #444444;
+}
+
+li {
+    left: 0;
+    top: 0;
+    list-style-type: none;
+    opacity: 0;
+    -webkit-transition: 1.55s ease-in-out;
+    -moz-transition: 1.55s ease-in-out;
+    -o-transition: 1.55s ease-in-out;
+    transition: 1.55s ease-in-out;
+}
+
+a {
+    text-decoration: none;
+    font-family: Varela Round;
+    color: white;
+    font-weight: bold;
+    font-size: 20px;
+}
+
+img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+}
+
+.active {
+    opacity: 1;
+    text-align: center;
+}
