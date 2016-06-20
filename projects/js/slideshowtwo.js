caa29a44bcb204b9db0aa0a752d0c59d4e6ffba1
Anish Kasam <iamalonerstalker@gmail.com>
2016-06-19 20:19:55 -0700

    Create slideshowtwo.js

---
+++ projects/js/slideshowtwo.js
@@ -0,0 +1,11 @@
+setInterval(function() {
+    debugger;
+    var active = $(".active").removeClass('active');
+    if (active.next() && active.next().length) {
+
+        active.next().addClass('active');
+
+    } else {
+        active.siblings(":first").addClass('active');
+    }
+}, 2000);
