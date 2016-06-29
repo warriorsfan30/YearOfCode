ab96d0045cc7edf440e01262882787ea25cf0f2f
Anish Kasam <iamalonerstalker@gmail.com>
2016-06-29 11:11:55 -0700

    Create bars.js

---
+++ projects/js/bars.js
@@ -0,0 +1,8 @@
+setInterval(function() {
+    var active = $(".active").removeClass('active');
+    if (active.next() && active.next().length) {
+        active.next().addClass('active');
+    } else {
+        active.siblings(":first").addClass('active');
+    }
+}, 375);
